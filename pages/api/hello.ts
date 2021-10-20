90597d6c87d8435a338330a5b7a15a90802e8478
4cenull <kuginail0324@gmail.com>
2021-10-20 20:59:25 +0900

    first commit

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,36 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { Sequelize, DataTypes } from 'sequelize'
 
-type Data = {
-  name: string
+function Hello (req: any, res: any) {
+  const connection = new Sequelize (
+    "postgres",
+    "postgres",
+    "jui8Hu5+PG",
+    {
+      host: 'localhost',
+      dialect: "postgres"
+    }
+  )
+
+  const test_table = connection.define("test_table", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING,
+    }
+  },{
+    timestamps: false,
+  });
+  test_table.sync();
+
+  //ユーザー情報の取得
+  test_table.findAll().then(function(value) {
+    res.statusCode = 200
+    res.json(value)
+  });
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
-}
+export default Hello
